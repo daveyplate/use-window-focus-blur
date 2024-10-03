04a7f5d6a7f00b8ec5562c84e0481ed0a556ba18
Davey <david@swiftapk.com>
2024-10-02 18:43:27 -0700

    Initial commit

---
+++ src/test-module.js
@@ -0,0 +1,13 @@
+/**
+ * Prints 'Hello World, ' + name to the console
+ * @param {string} name
+ * @type {(name: string) => void}
+ */
+export const helloWorld = (name) => {
+    console.log('Hello World, ' + name);
+}
+
+/** Test function */
+export function fooBar() {
+    console.log('fooBar');
+}
