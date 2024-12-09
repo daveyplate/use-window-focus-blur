8a306685a116316db4f115dc2a12016e4cd1379d
daveycodez <david@swiftapk.com>
2024-12-08 23:59:34 -0800

    TypeScript!

--- src/use-window-focus-blur.tsx
+++ src/use-window-focus-blur.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 /** Prevent the auto focus behaviour when the window is focused (blue outline fix) */
 export function useWindowFocusBlur() {
     useEffect(() => {
-        const handleFocus = () => document.activeElement.blur()
+        const handleFocus = () => (document.activeElement as HTMLElement)?.blur()
         window.addEventListener('focus', handleFocus)
 
         return () => window.removeEventListener('focus', handleFocus)
